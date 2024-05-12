5cf37a9e07fab762056c60cff737467251e51a01
LEE-SEON-WOO <x21999@naver.com>
2024-05-13 00:19:23 +0900

    seed change

--- Conv_Param_Exp/off_line/CMSIS_VS_DIRECT/template/X.py
+++ Conv_Param_Exp/off_line/CMSIS_VS_DIRECT/template/X.py
@@ -2,9 +2,8 @@ from fxpmath import Fxp
 import numpy as np
 from utils.utils import reproducibility
 from time import time
-import torch
-def input_format(size:tuple, bit=8):
-    reproducibility(1234)
+def input_format(size:tuple, bit=8, seed = time()):
+    reproducibility(int(seed))
     tensor = np.random.random(size)
     tensor = Fxp(tensor, signed=True, n_word=bit, overflow='saturate')
     tensor.config.dtype_notation = 'Q'
